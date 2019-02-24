90b83daaf23358333c7312235fe4202431b10c99
Carson Schubert <carson.schubert14@gmail.com>
2019-02-24 12:18:00 -0600

    Add setup structure.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='raven',
+    version='0.0.1',
+    description='Training CLI Tool',
+    packages=['raven'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+      [console_scripts]
+      raven=raven:cli
+    ''',
+)
+      
