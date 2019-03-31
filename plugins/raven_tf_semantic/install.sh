e15842e16d6ab54fff8e20cd6b4a04216e504e78
Carson Schubert <carson.schubert14@gmail.com>
2019-03-31 12:42:36 -0500

    fix(plugins): fix install issues

--- plugins/raven_tf_semantic/install.sh
+++ plugins/raven_tf_semantic/install.sh
@@ -33,6 +33,7 @@ if [ $install -eq 1 ]; then
     echo "Installing..."
     pip-compile --output-file $requirements_prefix.txt $requirements_prefix.in
     pip install -r $requirements_prefix.txt
+    pip install -e .
 else
     # NOTE: this does NOT clean up after the plugin (i.e, leaves plugin dependenices installed)
     # To clean up, use the install_all.sh script at the root of the plugins/ directory
