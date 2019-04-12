e36ef167666fb82d715b866b33aa8f700ac7afc4
Carson Schubert <carson.schubert14@gmail.com>
2019-04-11 23:08:53 -0500

    feat(cli): better feedback on clean command

--- ravenml/cli.py
+++ ravenml/cli.py
@@ -6,11 +6,14 @@ Main CLI entry point for ravenml.
 """
 
 import click
+from colorama import init, Fore
 from ravenml.utils.local_cache import global_cache
 from ravenml.train.commands import train
 from ravenml.data.commands import data
 from ravenml.config.commands import config
 from ravenml.utils.config import get_config, update_config
+
+init()
 
 ## OPTIONS
 clean_all_opt = click.option(
@@ -30,14 +33,19 @@ def clean(all):
     """ Cleans locally saved ravenml cache files.
     """
     if all:
-        global_cache.clean() 
+        if not global_cache.clean():
+            click.echo(Fore.RED + 'No cache to clean.')
     else:
         try:
             config = get_config()
             global_cache.clean()
             update_config(config)
-        except (ValueError, FileNotFoundError):
+        except ValueError:
+            click.echo(Fore.RED + 'Bad configuration file. Deleting alongside cache.') 
             global_cache.clean()
+        except FileNotFoundError:
+            if not global_cache.clean():
+                click.echo(Fore.RED + 'No cache to clean.')
 
 cli.add_command(train)
 cli.add_command(data)
