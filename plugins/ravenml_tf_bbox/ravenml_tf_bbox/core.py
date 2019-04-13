f3771ba0cb842b79657dab40e10964bea7ae06bb
Carson Schubert <carson.schubert14@gmail.com>
2019-04-13 17:08:24 -0500

    feat(ravenml_tf_bbox): setup dynamic importing of object detection and tensorflow
    avoids hefty imports happening when not needed

--- plugins/ravenml_tf_bbox/ravenml_tf_bbox/core.py
+++ plugins/ravenml_tf_bbox/ravenml_tf_bbox/core.py
@@ -2,15 +2,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import click
+import io
+import sys
+import importlib
+from absl import flags
 from ravenml.train.options import kfold_opt, pass_train
 from ravenml.train.interfaces import TrainInput, TrainOutput
-import tensorflow as tf
-from absl import flags
-from object_detection import model_hparams
-from object_detection import model_lib
 from utils.utils import prepare_for_training, download_model_arch
-import os
 
 @click.group(help='TensorFlow Object Detection with bounding boxes.')
 @click.pass_context
@@ -27,13 +27,14 @@ def train(ctx, train: TrainInput, kfold):
     # by Click because the @pass_train decorator is set to ensure
     # object creation, after which the created object is passed as "train"
     # after training, create an instance of TrainOutput and return it
-
+    _import_od()
+    
     tf.logging.set_verbosity(tf.logging.INFO)
-
+    
     data_path = train.dataset.get_dataset_path()
 
     base_dir = train.artifact_path
-    base_dir = '/Users/nihaldhamani/Desktop/test'
+    base_dir = '/home/carson/Desktop/test'
 
     # make into list for diff files
     arch_model_name = 'ssd_mobilenet_v1_coco_2018_01_28'
@@ -73,3 +74,56 @@ def train(ctx, train: TrainInput, kfold):
     result = TrainOutput()
 
     return result
+
+def _import_od():
+    """ Imports the necessary libraries for object detection training.
+    Used to avoid importing them at the top of the file where they get imported
+    on every ravenML command call, even those not to this plugin.
+    
+    Also suppresses warning outputs from the TF OD API.
+    """
+    # create a text trap and redirect stdout
+    # to suppress printed warnings from object detection and tf
+    text_trap = io.StringIO()
+    sys.stdout = text_trap
+    
+    # Calls to _dynamic_import below map to the following standard imports:
+    #
+    # import tensorflow as tf
+    # from object_detection import model_hparams
+    # from object_detection import model_lib
+    _dynamic_import('tensorflow', 'tf')
+    _dynamic_import('object_detection.model_hparams', 'model_hparams')
+    _dynamic_import('object_detection.model_lib', 'model_lib')
+    
+    # now restore stdout function
+    sys.stdout = sys.__stdout__
+    
+def _dynamic_import(modulename, shortname = None, asfunction = False):
+    """ Function to dynamically import python modules into the global scope.
+
+    Args:
+        modulename (str): name of the module to import (ex: os, ex: os.path)
+        shortname (str, optional): desired shortname binding of the module (ex: import tensorflow as tf)
+        asfunction (bool, optional): whether the shortname is a module function or not (ex: from time import time)
+        
+    Examples:
+        Whole module import: i.e, replace "import tensorflow"
+        >>> _dynamic_import('tensorflow')
+        
+        Named module import: i.e, replace "import tensorflow as tf"
+        >>> _dynamic_import('tensorflow', 'tf')
+        
+        Submodule import: i.e, replace "from object_detction import model_lib"
+        >>> _dynamic_import('object_detection.model_lib', 'model_lib')
+        
+        Function import: i.e, replace "from ravenml.utils.config import get_config"
+        >>> _dynamic_import('ravenml.utils.config', 'get_config', asfunction=True)
+        
+    """
+    if shortname is None: 
+        shortname = modulename
+    if asfunction is False:
+        globals()[shortname] = importlib.import_module(modulename)
+    else:        
+        globals()[shortname] = getattr(importlib.import_module(modulename), shortname)
