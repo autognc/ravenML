7cebfaea9ad44b4d2f3666fbed2e31a33cd23e60
Nihal Dhamani <nihaldhamani@Nihals-MacBook-Pro.local>
2019-06-18 19:59:59 -0700

    comment protoc command and update gitignore

--- plugins/ravenml_tf_bbox/install.sh
+++ plugins/ravenml_tf_bbox/install.sh
@@ -42,3 +42,10 @@ else
     echo "Uninstalling..."
     pip uninstall ravenml-tf-bbox -y
 fi
+
+
+# PROTOC_ZIP=protoc-3.7.1-osx-x86_64.zip
+# curl -OL https://github.com/google/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
+# sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+# sudo unzip -o $PROTOC_ZIP -d /usr/local include/*
+# rm -f $PROTOC_ZIP
