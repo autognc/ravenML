04adeac6cdc282a8523dc3d147df700517f1f820
Nihal Dhamani <nihaldhamani@Nihals-MacBook-Pro.local>
2019-06-19 19:03:06 -0700

    check for root in bbox install script

--- plugins/ravenml_tf_bbox/install.sh
+++ plugins/ravenml_tf_bbox/install.sh
@@ -37,8 +37,16 @@ if [ $install -eq 1 ]; then
         if [ $(uname) == 'Linux' ]; then
             PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
             curl -OL https://github.com/google/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
-            sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-            sudo unzip -o $PROTOC_ZIP -d /usr/local include/*
+            if [ `whoami` != 'root' ]
+                then
+                    sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+                    sudo unzip -o $PROTOC_ZIP -d /usr/local include/*
+
+                else
+                    unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+                    unzip -o $PROTOC_ZIP -d /usr/local include/*
+            fi
+            
             rm -f $PROTOC_ZIP 
         elif [ $(uname) == 'Darwin' ]; then
             PROTOC_ZIP=protoc-3.7.1-osx-x86_64.zip
