78666bc389ce1ca0333e0bb28794db8e7911879e
Carson Schubert <carson.schubert14@gmail.com>
2020-05-05 12:29:55 -0500

    Prototype using config files

--- ravenml/train/commands.py
+++ ravenml/train/commands.py
@@ -10,12 +10,15 @@ import json
 import shortuuid
 import os
 import boto3
+import yaml
+import sys
 from urllib.request import urlopen
 from urllib.error import URLError
 from pathlib import Path
 from pkg_resources import iter_entry_points
 from click_plugins import with_plugins
 from ravenml.train.interfaces import TrainInput, TrainOutput
+from ravenml.train.options import pass_train
 from ravenml.utils.dataset import get_dataset
 from ravenml.utils.question import cli_spinner
 from ravenml.utils.aws import upload_file_to_s3, upload_dict_to_s3_as_json
@@ -44,15 +47,21 @@ ec2_kill_opt = click.option(
     help='Do not kill EC2 instance ravenML is running on after training. Ignored if not on EC2.'
 )
 
+config_opt = click.option(
+    '-c', '--config', type=str, help='Path to config file. Defaults to ~/ravenML_configs/config.yaml'
+)
+
 ### COMMANDS ###
 @with_plugins(iter_entry_points('ravenml.plugins.train'))
 @click.group(help='Training commands.')
 @click.pass_context
-@ec2_kill_opt
-@dataset_opt
-@overwrite_local_opt
-@local_opt
-def train(ctx: click.Context, local: str, overwrite: bool, dataset: str, no_kill: bool):
+# @ec2_kill_opt
+# @dataset_opt
+# @overwrite_local_opt
+# @local_opt
+@config_opt
+# def train(ctx: click.Context, local: str, overwrite: bool, dataset: str, no_kill: bool):
+def train(ctx: click.Context, config: str):
     """ Training command group.
     
     Args:
@@ -74,20 +83,31 @@ def train(ctx: click.Context, local: str, overwrite: bool, dataset: str, no_kill
     #       a training, therefore NOT calling any other plugin subcommand. This means
     #       it is ok (and necessary) to create a TrainInput in this command, as we know
     #       for sure which plugin subcommand is coming next.o
-    if dataset or local:
+    if config:
         try:
-            ti = TrainInput(dataset_name=dataset, artifact_path=local, 
-                overwrite=overwrite, cache_name=ctx.invoked_subcommand)
-            # assign to context for use in plugin
-            ctx.obj = ti
-        except ValueError as e:
-            hint = 'dataset name, no such dataset exists on S3:'
-            raise click.exceptions.BadParameter(dataset, ctx=ctx, param=dataset, param_hint=hint)
+            with open(Path(config), 'r') as stream:
+                config = yaml.safe_load(stream)
+        except Exception as e:
+            hint = 'config, no such file exists'
+            raise click.exceptions.BadParameter(config, ctx=ctx, param=config, param_hint=hint)
+
+        ctx.obj = TrainInput(config, ctx.invoked_subcommand)
+
+    # if dataset or local:
+    #     try:
+    #         ti = TrainInput(dataset_name=dataset, artifact_path=local, 
+    #             overwrite=overwrite, cache_name=ctx.invoked_subcommand)
+    #         # assign to context for use in plugin
+    #         ctx.obj = ti
+    #     except ValueError as e:
+    #         hint = 'dataset name, no such dataset exists on S3:'
+    #         raise click.exceptions.BadParameter(dataset, ctx=ctx, param=dataset, param_hint=hint)
 
 @train.resultcallback()
+@pass_train
 @click.pass_context
-# def process_result(ctx: click.Context, result: TrainOutput, local: str, dataset: str):
-def process_result(ctx: click.Context, result: TrainOutput, local: str, overwrite: bool, dataset: str,  no_kill: bool):
+# def process_result(ctx: click.Context, result: TrainOutput, local: str, overwrite: bool, dataset: str,  no_kill: bool):
+def process_result(ctx: click.Context, ti: TrainInput, result: TrainOutput, config: str):
     """Processes the result of a training by analyzing the given TrainOutput object.
     This callback is called after ANY command originating from the train command 
     group, hence the check for commands other than training plugins.
@@ -98,18 +118,17 @@ def process_result(ctx: click.Context, result: TrainOutput, local: str, overwrit
         local (str): copy of local option provided to original command (see train)
         dataset (str): copy of dataset option provided to original comamand (see train)
     """
-
     # need to consider issues with this being called on every call to train
     if result is not None:
-        # upload if not in local mode
-        if not local:
+        # upload if not in local mode, determined by presence of artifact_path field in config
+        if not ti.config.get('artifact_path'):
             uuid = cli_spinner('Uploading artifacts...', _upload_result, result)
             click.echo(f'Artifact UUID: {uuid}')
         else:
             click.echo(f'LOCAL MODE: Not uploading model to S3. Model is located at: {result.artifact_path}')
             
         # kill if on ec2
-        if not no_kill:
+        if not ti.config.get('ec2_no_kill'):
             click.echo('Checking for EC2 instance...')
             try:
                 with urlopen(EC2_INSTANCE_ID_URL, timeout=5) as url:
@@ -120,7 +139,7 @@ def process_result(ctx: click.Context, result: TrainOutput, local: str, overwrit
             except URLError:
                 click.echo('No EC2 runtime detected.')
         else:
-            click.echo('Not checking for EC2 since --no-kill was passed.')
+            click.echo('Not checking for EC2 since no-kill was set.')
     return result
 
 
--- ravenml/train/interfaces.py
+++ ravenml/train/interfaces.py
@@ -5,16 +5,17 @@ Date Created:   03/03/2019
 Contains the classes for interfacing with training command group.
 """
 import os
+import sys
 import click
 import shutil
+from datetime import datetime
 from pathlib import Path
 from colorama import init, Fore
 from ravenml.utils.local_cache import RMLCache
 from ravenml.utils.question import cli_spinner, user_input, user_selects, user_confirms
 from ravenml.utils.dataset import get_dataset_names, get_dataset
 from ravenml.data.interfaces import Dataset
-
-init()
+from schema import Schema, Optional, And, Use
 
 class TrainInput(object):
     """Represents a training input. Contains all plugin-independent information
@@ -32,7 +33,70 @@ class TrainInput(object):
         artifact_path (Path): path to save artifacts. None if uploading to s3
         plugin_cache (RMLCache): RMLCache for this plugin. Created at 
             ~/.ravenML/<plugin command>.
+        config (dict)
     """
+    
+   
+    def __init__(self):
+        """ Default constructor for use with @pass_train pass decorator
+        Automatically called when a command decorated with @pass_train is
+        triggered and not TrainInput exists in ctx.obj. This means the user
+        did not provide a config in the --config option.
+        """
+        raise click.exceptions.UsageError(('You must provide the --config option'
+            'on `ravenml train` when using this plugin command.'))
+
+    def __init__(self, config: dict, cache_name:str):
+        ## Set up Local Cache ##
+        self.plugin_cache = RMLCache(cache_name)
+        
+        ## Set up Artifact Path
+        ap = config.get('artifact_path')
+        if ap is None:
+            self.plugin_cache.ensure_clean_subpath('temp')
+            self.plugin_cache.ensure_subpath_exists('temp')
+            self.artifact_path = Path(self.plugin_cache.path / 'temp')
+        else:
+            ap = Path(artifact_path)
+            # check if local path contains data
+            if os.path.exists() and os.path.isdir(ap) and len(os.listdir(ap)) > 0:
+                if config.get('overwrite_local') or user_confirms('Local artifact storage location contains old data. Overwrite?'):
+                    shutil.rmtree(ap)
+                else:
+                    click.echo(Fore.RED + 'Training cancelled.')
+                    click.get_current_context().exit() 
+            # create directory, need exist_ok since we only delete
+            # if directory contains files
+            os.makedirs(ap, exist_ok=True)
+            self.artifact_path = ap
+        
+        ## Set up Dataset ##
+        # prompt for dataset if not provided
+        dataset_name = config.get('dataset')
+        if dataset_name is None:
+            dataset_options = cli_spinner('No dataset provided. Finding datasets on S3...', get_dataset_names)
+            dataset_name = user_selects('Choose dataset:', dataset_options)
+        # download dataset and populate field
+        try:
+            self.dataset = cli_spinner(f'Downloading {dataset_name} from S3...', 
+                get_dataset, dataset_name)
+        except ValueError as e:
+            hint = 'dataset name, no such dataset exists on S3'
+            raise click.exceptions.BadParameter(dataset_name, param=dataset_name, param_hint=hint)
+    
+        ## Set up Basic Metadata ##
+        # TODO: add environment description, git hash, etc
+        self.metadata = config.get('metadata', {})
+        # handle user defined metadata fields
+        if not self.metadata.get('created_by'):
+            self.metadata['created_by'] = user_input('Please enter your first and last name:')
+        if not self.metadata.get('comments'):
+            self.metadata['comments'] = user_input('Please enter descriptive comments about this training:')
+        
+        # handle automatic metadata fields
+        self.metadata['date_started_at'] = datetime.utcnow().isoformat() + "Z"
+        self.metadata['dataset_used'] = self.dataset.metadata
+            
     
     ## NOTE: ##
     # Constructor uses only kwargs to make it compatible for use with
@@ -43,45 +107,45 @@ class TrainInput(object):
     # ONLY when arguments are passed, which allows --help and other plugin
     # subcommands to be unaffected by TrainInput construction when a training
     # is not actually being started.
-    def __init__(self, dataset_name=None, artifact_path=None, overwrite=False, cache_name=None):
-        ## Set up Local Cache ##
-        # cache name is None when __init__ called by pass decorator, so get plugin name via context
-        if cache_name is None:
-            self.plugin_cache = RMLCache(click.get_current_context().command_path.split(' ')[2]) 
-        else:
-            self.plugin_cache = RMLCache(cache_name)
+    # def __init__(self, dataset_name=None, artifact_path=None, overwrite=False, cache_name=None):
+    #     ## Set up Local Cache ##
+    #     # cache name is None when __init__ called by pass decorator, so get plugin name via context
+    #     if cache_name is None:
+    #         self.plugin_cache = RMLCache(click.get_current_context().command_path.split(' ')[2]) 
+    #     else:
+    #         self.plugin_cache = RMLCache(cache_name)
         
-        ## Set up Artifact Path ##
-        # path is None for s3 upload (no local flag passed)
-        if artifact_path is None:
-            # clear temp dir in case already exists
-            self.plugin_cache.ensure_clean_subpath('temp')
-            # create temp dir in local cache for artifacts
-            self.plugin_cache.ensure_subpath_exists('temp')
-            self.artifact_path = Path(self.plugin_cache.path / 'temp')
-        else:
-            # convert to Path object
-            ap = Path(artifact_path)
-            # check if local path contains data
-            if os.path.exists(ap) and os.path.isdir(ap) and len(os.listdir(ap)) > 0:
-                if overwrite or user_confirms('Artifact storage location contains old data. Overwrite?'):
-                    shutil.rmtree(ap)
-                else:
-                    click.echo(Fore.RED + 'Training cancelled.')
-                    click.get_current_context().exit() 
-            # create directory, need exist_ok since we only delete
-            # if the directory contains files
-            os.makedirs(ap, exist_ok=True)
-            self.artifact_path = ap
+    #     ## Set up Artifact Path ##
+    #     # path is None for s3 upload (no local flag passed)
+    #     if artifact_path is None:
+    #         # clear temp dir in case already exists
+    #         self.plugin_cache.ensure_clean_subpath('temp')
+    #         # create temp dir in local cache for artifacts
+    #         self.plugin_cache.ensure_subpath_exists('temp')
+    #         self.artifact_path = Path(self.plugin_cache.path / 'temp')
+    #     else:
+    #         # convert to Path object
+    #         ap = Path(artifact_path)
+    #         # check if local path contains data
+    #         if os.path.exists(ap) and os.path.isdir(ap) and len(os.listdir(ap)) > 0:
+    #             if overwrite or user_confirms('Artifact storage location contains old data. Overwrite?'):
+    #                 shutil.rmtree(ap)
+    #             else:
+    #                 click.echo(Fore.RED + 'Training cancelled.')
+    #                 click.get_current_context().exit() 
+    #         # create directory, need exist_ok since we only delete
+    #         # if the directory contains files
+    #         os.makedirs(ap, exist_ok=True)
+    #         self.artifact_path = ap
             
-        ## Set up Dataset ##
-        # prompt for dataset if not provided
-        if dataset_name is None:
-            dataset_options = cli_spinner('Finding datasets on S3...', get_dataset_names)
-            dataset_name = user_selects('Choose dataset:', dataset_options)
-        # download dataset and populate field
-        self.dataset = cli_spinner(f'Downloading {dataset_name} from S3...', 
-            get_dataset, dataset_name)
+    #     ## Set up Dataset ##
+    #     # prompt for dataset if not provided
+    #     if dataset_name is None:
+    #         dataset_options = cli_spinner('Finding datasets on S3...', get_dataset_names)
+    #         dataset_name = user_selects('Choose dataset:', dataset_options)
+    #     # download dataset and populate field
+    #     self.dataset = cli_spinner(f'Downloading {dataset_name} from S3...', 
+    #         get_dataset, dataset_name)
 
 class TrainOutput(object):
     """Training Output class
