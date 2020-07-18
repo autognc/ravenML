5fb4f7feeefc505db7cee0db7b8312bc966d9c92
Daniel Fonseka <dlfonseka@utexas.edu>
2020-07-17 23:21:41 -0500

    added missing quotation marks in interface files

--- ravenml/train/interfaces.py
+++ ravenml/train/interfaces.py
@@ -71,7 +71,7 @@ class TrainInput(object):
         ## Set up Local Cache
         # TODO: maybe create the subdir here?
         # currently the cache_name subdir is only created IF the plugin places files there
-        self.plugin_cache = RMLCache(f'train_{plugin_name})
+        self.plugin_cache = RMLCache(f'train_{plugin_name}')
         
         ## Set up Artifact Path
         ap = config.get('artifact_path')
