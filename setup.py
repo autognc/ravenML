d4a5d7165a9cb7978db15c4bb83ef66a90ad171b
Carson Schubert <carson.schubert14@gmail.com>
2020-11-10 14:29:10 -0800

    test package data

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     keywords= ['machine learning', 'data science'],
     download_url = 'https://github.com/autognc/ravenML/archive/v1.2.tar.gz',
     packages=find_packages(),
-    package_data={'ravenml': ['./setup.cfg']},
+    package_data={'ravenml': ['./Dev_Intro.md']},
     install_requires=[
         'Click>=7.0',
         'questionary>=1.0.2',
